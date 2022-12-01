3972772ea2f0c647284c0927c3e52676151d6549
preska <wolfwar@outlook.com>
2022-12-01 20:03:06 +0100

    hero and about

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => (
         className="w-[24px] h-[24px] object-contain"
       />
       <h2 className='="font-extrabold text-[24px] leading-[30px] text-white'>
-        META
+        METAVERSUS
       </h2>
 
       <img
